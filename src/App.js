b1df935547aa63ea0e8fe27a8f20091d360ae09d
Laurent Grima <grima.laurent@gmail.com>
2017-11-22 23:57:44 +0100

    Create dumb form and give it some styling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import './App.css';
 type Props = void;
 
 class App extends Component<Props> {
-	render() {
-		return <div>coucou</div>;
-	}
+  render() {
+    return (
+      <div className="App">
+        <div className="App__Card">
+          <div className="App__Card__Title">
+            <h1>Hi there</h1>
+            <p>Chose an image you want our bot to describe.</p>
+          </div>
+          <form className="App__Card__Content">
+            <div className="Input">
+              <label>Your file</label>
+              <input type="file" />
+            </div>
+            <button className="Button">Describe</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
