5ca0c1fcb24cfeef59437ceba22464277d177fd8
Laurent Grima <grima.laurent@gmail.com>
2017-11-22 19:02:03 +0100

    Add scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 // @flow
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 type Props = void;
 
 class App extends Component<Props> {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return <div>coucou</div>;
+	}
 }
 
 export default App;
